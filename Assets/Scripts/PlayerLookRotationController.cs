410692ab202188a6213664a9694f4fe77a25b083
Jaween Ediriweera <jaween.ediriweera@gmail.com>
2020-02-01 12:46:43 +1030

    Add player movement rotation, increase planet size

---
+++ Assets/Scripts/PlayerLookRotationController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLookRotationController : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        
+        Vector3 inputDir = new Vector3(horizontal, vertical, 0).normalized;
+        transform.localRotation = Quaternion.Euler(inputDir.x, inputDir.y, 0);
+        float angle = Mathf.Atan2(horizontal, vertical) * Mathf.Rad2Deg;
+        transform.Rotate(Vector3.up, angle);
+    }
+}
